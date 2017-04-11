e7960e1b8c6286c005a65d600e23fb2b332dfa3f
r-sede <r_sede@orange.fr>
2017-04-11 10:34:34 +0200

    affiche les pions

--- js/app.js
+++ js/app.js
@@ -14,6 +14,21 @@ function afficheTableauVide(w,h,id){
 
 }
 
+function afficheLesPions(pionArr,players){
+	$('.cell').html('&nbsp');
+
+	for(var i = 0;i < pionArr.length; i++){
+		var cOwn = pionArr[i].owner;
+		var cX = pionArr[i].posX;
+		var cY = pionArr[i].posY;
+
+		var valString = cX+''+cY;
+
+		$(".cell[value='"+valString+"']").html(players[cOwn]);
+
+	}
+}
+
 var Pion = function (x,y,owner,pionArr){
 	this.posX=x;
 	this.posY=y;
@@ -25,6 +40,8 @@ var Pion = function (x,y,owner,pionArr){
 
 
 $(document).ready(function(){
+var joueurs = ["X","O"];
+
 
 afficheTableauVide(3,3,"#maVue");
 
@@ -34,8 +51,11 @@ var pionArray = [];
 var turn = true;
 
 console.log(pionArray);
-var test = new Pion(0,0,'j1',pionArray);
+var test = new Pion(0,0,0,pionArray);
+var test2 = new Pion(0,2,1,pionArray);
+
 console.log("after New Pion", pionArray);
+afficheLesPions(pionArray,joueurs);
 
 
 });
