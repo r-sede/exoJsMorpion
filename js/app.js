1f1fadb8e14182cda53efcff1d37134ce761f9f6
r-sede <r_sede@orange.fr>
2017-04-11 10:07:57 +0200

    display html table td have value=xy + css border

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('hello world!!');
+function afficheTableauVide(w,h,id){
+
+	$(id).html('');
+
+	for(var yy = 0; yy < h ; yy++){
+		var curLigne = "<tr>";
+		for(var xx = 0 ;xx < w ; xx++){
+			var valuString = xx+""+yy;
+			curLigne+='<td class="cell" value="'+valuString+'"></td>';
+		}
+		curLigne +="</tr>";
+		$(id).append(curLigne);
+	}
+
+}
+
+
+
+
+$(document).ready(function(){
+
+afficheTableauVide(3,3,"#maVue");
+
+});
