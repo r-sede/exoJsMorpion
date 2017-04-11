2924c204211b9164e6b94117d621f01a7078089b
r-sede <r_sede@orange.fr>
2017-04-11 11:24:33 +0200

    alterne j1 j2, click add pion n remove listenr todo checkConditio

--- js/app.js
+++ js/app.js
@@ -37,25 +37,52 @@ var Pion = function (x,y,owner,pionArr){
 
 };
 
+function whichPlayer(turnNumb){
+	return turnNumb %2;
+}
 
 
 $(document).ready(function(){
+//carac representant les pions des joueurs
 var joueurs = ["X","O"];
+
+//liste de pions en jeu
+var pionArray = [];
+
+//j1 =true j2 = false;
+var turn = 0;
 
 
 afficheTableauVide(3,3,"#maVue");
 
-var pionArray = [];
 
-//j1 =true j2 = false;
-var turn = true;
+//test !!!
+// console.log(pionArray);
+// var test = new Pion(0,0,0,pionArray);
+// var test2 = new Pion(0,2,1,pionArray);
 
-console.log(pionArray);
-var test = new Pion(0,0,0,pionArray);
-var test2 = new Pion(0,2,1,pionArray);
-
-console.log("after New Pion", pionArray);
+// console.log("after New Pion", pionArray);
 afficheLesPions(pionArray,joueurs);
+
+
+
+$('.cell').on('click',function(){
+	// console.log('.cell click');
+	var tt = $(this).attr('value');
+	console.log(tt);
+	
+	var npX = tt[0];
+	var npY	= tt[1];
+
+	var nPion = new Pion(npX,npY,whichPlayer(turn),pionArray);
+	//checkVictory
+	turn ++;
+	//remove handler
+	$(this).off();
+	afficheLesPions(pionArray,joueurs);
+
+});
+/////test
 
 
 });
