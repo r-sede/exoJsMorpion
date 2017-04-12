1c45a0f49946ce50b7aba6ec280325283966fe61
r-sede <r_sede@orange.fr>
2017-04-12 10:27:52 +0200

    fin exo Raf

--- js/app.js
+++ js/app.js
@@ -14,119 +14,120 @@ function afficheTableauVide(w,h,id){
 
 }
 
-function afficheLesPions(pionArr,players){
+function afficheLesPions(){
 	$('.cell').html('&nbsp');
 
-	for(var i = 0;i < pionArr.length; i++){
-		var cOwn = pionArr[i].owner;
-		var cX = pionArr[i].posX;
-		var cY = pionArr[i].posY;
+	for(var i = 0;i < pionArray.length; i++){
+		var cOwn = pionArray[i].owner;
+		var cX = pionArray[i].posX;
+		var cY = pionArray[i].posY;
 
 		var valString = cX+''+cY;
 
-		$(".cell[value='"+valString+"']").html(players[cOwn]);
+		$(".cell[value='"+valString+"']").html(joueurs[cOwn]);
 
 	}
 }
 
-var Pion = function (x,y,owner,pionArr){
+function checkVictoire(){
+
+	var tables = 	[['','',''],
+					 ['','',''],
+					 ['','','']];
+
+	for(var i = 0; i< pionArray.length;i++){
+		var cOwn = pionArray[i].owner;
+		var cX = pionArray[i].posX;
+		var cY = pionArray[i].posY;
+
+		tables[cY][cX] = joueurs[cOwn];
+		console.log(tables);
+	}
+	
+
+	var patternTocheck =joueurs[whichPlayer()]+""+joueurs[whichPlayer()]+""+joueurs[whichPlayer()];
+
+	var ligne1 = tables[0].join('');
+	var ligne2 = tables[1].join('');
+	var ligne3 = tables[2].join('');
+	
+	var col1 = tables[0][0]+""+tables[1][0]+""+tables[2][0];
+	var col2 = tables[0][1]+""+tables[1][1]+""+tables[2][1];
+	var col3 = tables[2][0]+""+tables[2][1]+""+tables[2][2];
+
+	var diag1 = tables[0][0]+""+tables[1][1]+""+tables[2][2];
+	var diag2 = tables[2][0]+""+tables[1][1]+""+tables[0][2];
+
+	var megafun = [ligne1,ligne2,ligne3,col1,col2,col3,diag1,diag2];
+
+	for(var p = 0; p<megafun.length;p++){
+		// console.log("megafun",megafun[p]);
+		if(megafun[p].includes(patternTocheck)){
+			return true;
+		}
+	}
+
+	return false;
+
+
+
+}
+
+var Pion = function (x,y,owner,pioArr){
 	this.posX=x;
 	this.posY=y;
 	this.owner= owner;
-	pionArr.push(this);
+	pioArr.push(this);
 
 };
 
-function whichPlayer(turnNumb){
-	return turnNumb %2;
+function whichPlayer(){
+	return turn %2;
 }
 
-function checkWin(){
 
-	var table = [['S','S','S'], ['S','S','S'], ['S','S','S']];
-	for (var i = 0; i < pionArray.length; i++) {
-		var cx = pionArray[i].posX;
-		var cy = pionArray[i].posY;
-		var ownjoueur = pionArray[i].owner;
-		console.log(cx,cy);
-
-		table[cy][cx] = joueurs[ownjoueur];
-	}
-			
-	
-		// verifie les ligne du tableau
-		if (table[0][0] == table[0][1] && table[0][0] == table[0][2]) {
-			alert("vous aver gagner");
-		}
-		else if(table[1][0] == table[1][1] && table[1][0] == table[1][2] && table[1][0] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-
-		else if(table[2][0] == table[2][1] && table[2][0] == table[2][2] && table[2][0] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-
-		//Verifie les colonnes du tableau
-		else if(table[0][0] == table[1][0] && table[0][0] == table[2][0] && table[0][0] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-		else if(table[0][1] == table[1][1] && table[0][1] == table[2][1] && table[0][1] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-		else if(table[0][2] == table[1][2] && table[0][2] == table[2][2] && table[0][2] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-
-		//Verifie les diagonales du tableau
-		else if(table[0][0] == table[1][1] && table[0][0] == table[2][2] && table[0][0] == joueurs[whichPlayer(turn)]){
-		alert("vous aver gagner");
-		}
-		else if(table[0][2] == table[1][1] && table[0][2] == table[2][0] && table[1][0] == joueurs[whichPlayer(turn)]){
-			alert("vous aver gagner");
-		}	
-	
-		
-
-}
-//liste de pions en jeu
-var pionArray = [];
 //carac representant les pions des joueurs
 var joueurs = ["X","O"];
 
-//j1 =true j2 = false;
+//liste de pions en jeu
+var pionArray = [];
+
+//compteur de tour 
 var turn = 0;
+
 
 $(document).ready(function(){
 
 
+	afficheTableauVide(3,3,"#maVue");
 
 
-afficheTableauVide(3,3,"#maVue");
-
-
-afficheLesPions(pionArray,joueurs);
-
-
-
-$('.cell').on('click',function(){
-	// console.log('.cell click');
-	var tt = $(this).attr('value');
-	console.log(tt);
-	
-	var npX = tt[0];
-	var npY	= tt[1];
-
-	var nPion = new Pion(npX,npY,whichPlayer(turn),pionArray);
-	//checkVictory !!! manque plus que ca !!!!
-	checkWin();
-	turn ++;
-	
-	//remove listener pour eviter de placer un pion sur une case occupé
-	$(this).off();
 	afficheLesPions(pionArray,joueurs);
 
-});
-/////test
 
 
-});
+	$('.cell').on('click',function(){
+		// console.log('.cell click');
+		var tt = $(this).attr('value');
+		// console.log(tt);
+		
+		var npX = tt[0];
+		var npY	= tt[1];
+
+		var nPion = new Pion(npX,npY,whichPlayer(turn),pionArray);
+		console.log(nPion);
+		if(checkVictoire()){
+			alert('joueur '+whichPlayer()+' a gagné');
+			window.location.reload();
+			// console.log(pionArray);
+		}
+		turn ++;
+		//remove handler
+		$(this).off();
+		afficheLesPions();
+
+	});//.cell on click
+
+
+
+});//document.ready
