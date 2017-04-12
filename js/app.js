f7e9b0948c144838c5bcab1a1cf5c28d169ef00a
r-sede <r_sede@orange.fr>
2017-04-12 10:01:00 +0200

    commit before new Branch

--- js/app.js
+++ js/app.js
@@ -14,75 +14,120 @@ function afficheTableauVide(w,h,id){
 
 }
 
-function afficheLesPions(pionArr,players){
+function afficheLesPions(){
 	$('.cell').html('&nbsp');
 
-	for(var i = 0;i < pionArr.length; i++){
-		var cOwn = pionArr[i].owner;
-		var cX = pionArr[i].posX;
-		var cY = pionArr[i].posY;
+	for(var i = 0;i < pionArray.length; i++){
+		var cOwn = pionArray[i].owner;
+		var cX = pionArray[i].posX;
+		var cY = pionArray[i].posY;
 
 		var valString = cX+''+cY;
 
-		$(".cell[value='"+valString+"']").html(players[cOwn]);
+		$(".cell[value='"+valString+"']").html(joueurs[cOwn]);
 
 	}
 }
 
-var Pion = function (x,y,owner,pionArr){
+function checkVictoire(){
+
+	var tables = 	[['','',''],
+					 ['','',''],
+					 ['','','']];
+
+	for(var i = 0; i< pionArray.length;i++){
+		var cOwn = pionArray[i].owner;
+		var cX = pionArray[i].posX;
+		var cY = pionArray[i].posY;
+
+		tables[cY][cX] = joueurs[cOwn];
+		console.log(tables);
+	}
+	
+
+	var patternTocheck =joueurs[whichPlayer()]+""+joueurs[whichPlayer()]+""+joueurs[whichPlayer()];
+
+	var ligne1 = tables[0].join('');
+	var ligne2 = tables[1].join('');
+	var ligne3 = tables[2].join('');
+	
+	var col1 = tables[0][0]+""+tables[1][0]+""+tables[2][0];
+	var col2 = tables[0][1]+""+tables[1][1]+""+tables[2][1];
+	var col3 = tables[2][0]+""+tables[2][1]+""+tables[2][2];
+
+	var diag1 = tables[0][0]+""+tables[1][1]+""+tables[2][2];
+	var diag2 = tables[2][0]+""+tables[1][1]+""+tables[0][2];
+
+	var megafun = [ligne1,ligne2,ligne3,col1,col2,col3,diag1,diag2];
+
+	for(var p = 0; p<megafun.length;p++){
+		// console.log("megafun",megafun[p]);
+		if(megafun[p].includes(patternTocheck)){
+			return true;
+		}
+	}
+
+	return false;
+
+
+
+}
+
+var Pion = function (x,y,owner,pioArr){
 	this.posX=x;
 	this.posY=y;
 	this.owner= owner;
-	pionArr.push(this);
+	pioArr.push(this);
 
 };
 
-function whichPlayer(turnNumb){
-	return turnNumb %2;
+function whichPlayer(){
+	return turn %2;
 }
 
 
-$(document).ready(function(){
 //carac representant les pions des joueurs
 var joueurs = ["X","O"];
 
 //liste de pions en jeu
 var pionArray = [];
 
-//j1 =true j2 = false;
+//compteur de tour 
 var turn = 0;
 
 
-afficheTableauVide(3,3,"#maVue");
+$(document).ready(function(){
 
 
-//test !!!
-// console.log(pionArray);
-// var test = new Pion(0,0,0,pionArray);
-// var test2 = new Pion(0,2,1,pionArray);
-
-// console.log("after New Pion", pionArray);
-afficheLesPions(pionArray,joueurs);
+	afficheTableauVide(3,3,"#maVue");
 
 
-
-$('.cell').on('click',function(){
-	// console.log('.cell click');
-	var tt = $(this).attr('value');
-	console.log(tt);
-	
-	var npX = tt[0];
-	var npY	= tt[1];
-
-	var nPion = new Pion(npX,npY,whichPlayer(turn),pionArray);
-	//checkVictory
-	turn ++;
-	//remove handler
-	$(this).off();
 	afficheLesPions(pionArray,joueurs);
 
-});
-/////test
 
 
-});
+	$('.cell').on('click',function(){
+		// console.log('.cell click');
+		var tt = $(this).attr('value');
+		// console.log(tt);
+		
+		var npX = tt[0];
+		var npY	= tt[1];
+
+		var nPion = new Pion(npX,npY,whichPlayer(turn),pionArray);
+		console.log(nPion);
+		if(checkVictoire()){
+			alert('joueur '+whichPlayer()+' a gagné');
+			window.location.reload();
+			// console.log(pionArray);
+		}
+		turn ++;
+		//remove handler
+		$(this).off();
+		afficheLesPions();
+
+	});//.cell on click
+
+
+
+});//document.ready
